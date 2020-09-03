799941681e47bd0a6f3a65c9752dbf11ca247356
Ashwin Bhaskar <ashwin.bhaskar@go-jek.com>
2020-09-03 15:57:10 +0530

    Ability to configure indexes and bins with different transformers

--- src/aerospike_migration/relation.clj
+++ src/aerospike_migration/relation.clj
@@ -1,27 +1,42 @@
 (ns aerospike-migration.relation
-  (:require [clojure.java.io :as jio]
-            [failjure.core :as f]
-            [aerospike-migration.util :as u]
+  (:require [aerospike-migration.util :as u]
             [next.jdbc :as j]
-            [aerospike-migration.postgres :refer [ds]]))
+            [aerospike-migration.postgres :refer [ds]]
+            [aerospike-migration.spec :as s]
+            [clojure.string :as str]
+            [camel-snake-kebab.core :as csk]
+            [aerospike-clj.client :as aero]
+            [aerospike-migration.aerospike :refer [client]]))
 
-(def mapping (atom {}))
+(defn- prepare-query
+  [columns relation]
+  (format "SELECT %s FROM %s" (str/join "," columns) relation))
 
 (defn- column-name->bin-name
-  [relation column-name]
-  (-> @mapping
+  [mapping relation column-name]
+  (-> mapping
       relation
-      ((keyword column-name))
-      :bin))
+      column-name
+      ::s/bin))
+
+(defn- column-value->bin-value
+  [mapping relation column-name value]
+  (let [transformer (-> mapping
+                        relation
+                        column-name
+                        ::s/function
+                        u/functions)]
+    (transformer value)))
 
 (defn- column->bin
-  [relation column-name value]
-  {(column-name->bin-name relation column-name) value})
+  [mapping relation column-name value]
+  {(column-name->bin-name mapping relation column-name)
+   (column-value->bin-value mapping relation column-name value)})
 
 (defn- row->bin
-  [row relation]
+  [row mapping relation]
   (reduce (fn [acc [k v]]
-            (into acc (column->bin relation k v)))
+            (into acc (column->bin mapping relation k v)))
           {}
           row))
 
@@ -31,18 +46,43 @@
                (assoc acc k (u/pg-object->map v)))]
     (reduce r-fn {} row)))
 
-(defn migrate-relation
-  [relation]
-  (let [query (u/prepare-query @mapping relation)]
+(defn- index
+  [row pk-info]
+  (let [delimiter (::s/pk-info.delimiter pk-info)
+        prepend   (u/if-not-blank (::s/pk-info.prepend pk-info)
+                                  #(str % delimiter))
+        append    (u/if-not-blank (::s/pk-info.append pk-info)
+                                  #(str delimiter %))
+        mapper    (fn [k]
+                    (let [transformer (u/functions (get-in pk-info [k ::s/function]))]
+                      (-> (k row)
+                          transformer)))
+        pks       (->> (map mapper (::s/pk-info.primary-keys pk-info))
+                       (str/join delimiter))]
+    (str prepend
+         pks
+         append)))
+
+(defn- migrate-relation
+  [mapping [relation v]]
+  (let [columns          (set (::s/columns v))
+        columns-in-query (->> (map name columns)
+                              (map #(csk/->snake_case %)))
+        query            (prepare-query columns-in-query (name relation))]
     (->> (j/execute! ds [query])
          (map u/kebab-caseize)
          (map sanitize-row)
-         (map #(row->bin % relation)))))
+         (map (fn [row]
+                (let [index-keys (-> (select-keys row (get-in v [::s/pk-info ::s/pk-info.primary-keys]))
+                                     keys)
+                      _          (println (str "index keys " index-keys))
+                      data       (row->bin (apply #(dissoc row %) index-keys) mapping relation)
+                      index      (index row (::s/pk-info v))]
+                  {:data  data
+                   :index index})))
+         (run! (fn [{:keys [data index]}]
+                 (aero/put client index "users" data -1))))))
 
 (defn migrate
-  [relation edn-filepath batch-size]
-  (with-open [failure-logger (jio/writer (format "%s-failed-rows" relation))]
-    (f/attempt-all [mappings         (u/load-edn edn-filepath)
-                    _                (reset! mapping mappings)
-                    relation-mapping (relation mappings)]
-      ())))
+  [m]
+  (run! #(migrate-relation m %) m))
