54079576b2fd3658bbd46cb6e5f900feba7bcaf6
Ashwin Bhaskar <ashwin.bhaskar@go-jek.com>
2020-09-03 09:19:52 +0530

    Complete spec for edn except primary key

--- src/aerospike_migration/spec.clj
+++ src/aerospike_migration/spec.clj
@@ -2,6 +2,27 @@
   (:require [clojure.spec.alpha :as spec]
             [clojure.spec.alpha :as s]))
 
-(spec/def ::columns sequential?)
+(spec/def ::columns (spec/and sequential?
+                              #(every? keyword? %)))
 
-(spec/def ::relation (s/keys :req [::columns]))
+(spec/def ::function #{:identity :timestamp->epoch-seconds})
+
+(spec/def ::bin string?)
+
+(spec/def ::column (spec/keys :req [::bin ::function]))
+
+(spec/def ::validate-columns (fn [m]
+                               (let [columns (::columns m)]
+                                 (->> (dissoc m ::columns ::pk)
+                                      (every? (fn [[k v]]
+                                                (and (some #(= k %) columns)
+                                                     (spec/valid? ::column v))))))))
+
+(spec/def ::relation (spec/and (spec/keys :req [::columns])
+                               ::validate-columns))
+(spec/def ::root #(every? (fn [[k v]]
+                           (and (keyword? k)
+                                (s/valid? ::relation v)))
+                          %))
+
+
