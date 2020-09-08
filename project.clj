a3e9a0b89b23c7391869e03a11d4c0da3d8f8bd2
Ashwin Bhaskar <ashwin.bhaskar@go-jek.com>
2020-09-08 17:39:19 +0530

    Add exclusions suggested by lein deps :tree

--- project.clj
+++ project.clj
@@ -4,10 +4,10 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [aerospike-clj "1.0.0"]
+                 [aerospike-clj "1.0.0" :exclusions [org.clojure/clojure]]
                  [mount "0.1.16"]
                  [cli-matic "0.4.3"]
-                 [seancorfield/next.jdbc "1.1.582"]
+                 [seancorfield/next.jdbc "1.1.582" :exclusions [org.clojure/clojure]]
                  [hikari-cp "2.13.0"]
                  [org.postgresql/postgresql "42.2.16"]
                  [camel-snake-kebab "0.4.1"]
