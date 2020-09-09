dbcbe3b325e3d9fb22552fa1bd1864d16dccac0e
Ashwin Bhaskar <ashwin.bhaskar@go-jek.com>
2020-09-09 05:42:42 +0530

    Change clojure dependency to alpha-1 and remove hikari cp

--- project.clj
+++ project.clj
@@ -3,12 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.2-alpha1"]
                  [aerospike-clj "1.0.0" :exclusions [org.clojure/clojure]]
                  [mount "0.1.16"]
                  [cli-matic "0.4.3"]
                  [seancorfield/next.jdbc "1.1.582" :exclusions [org.clojure/clojure]]
-                 [hikari-cp "2.13.0"]
                  [org.postgresql/postgresql "42.2.16"]
                  [camel-snake-kebab "0.4.1"]
                  [ch.qos.logback/logback-classic "1.2.3"]
